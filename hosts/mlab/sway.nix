f2ab062f7116215e9c8c2b34e34dbd46aee4b713
themarcel <code@marcel.cool>
2026-06-22 20:24:21 +0200

    properly setup sway for mlab

--- hosts/mlab/sway.nix
+++ hosts/mlab/sway.nix
@@ -21,7 +21,7 @@
       slurp
       swappy
       libnotify
-      bluetuith # TUI: scan/pair/trust/connect new devices
+      bluetuith # bluetooth tui
     ];
   };
   security.polkit.enable = true;
@@ -36,32 +36,26 @@
       config = {
         modifier = "Mod1";
         terminal = "foot";
-        menu = "tofi-run";
-        # 4K on HDMI-A-1; scale matches hyprland monitors.conf (1.67).
+        menu = "tofi-run | xargs -r swaymsg exec --";
         output."HDMI-A-1" = {
           resolution = "3840x2160@60Hz";
           scale = "1.67";
         };
         keybindings = lib.mkOptionDefault {
-          "Mod1+Q" = "exec brave-origin-nightly";
-          "Mod1+D" = "exec tofi-run";
-          "Mod1+B" = "exec ~/scripts/bluetooth-selector.sh";
-          "Mod1+Shift+B" = "exec foot -e bluetuith";
+          "Mod1+q" = "exec brave-origin-nightly";
+          "Mod1+b" = "exec ~/scripts/bluetooth-selector.sh";
+          "Mod1+Shift+b" = "exec foot -e bluetuith";
           "Mod1+Print" = ''exec grim -g "$(slurp)" - | swappy -f -'';
-
-          "Mod1+Shift+Q" = "kill";
-          "Mod1+F" = "fullscreen toggle";
-          "Mod1+V" = "floating toggle";
-          "Mod1+Shift+R" = "reload";
-
-          "Mod1+H" = "focus left";
-          "Mod1+J" = "focus down";
-          "Mod1+K" = "focus up";
-          "Mod1+L" = "focus right";
-          "Mod1+Shift+H" = "move left";
-          "Mod1+Shift+J" = "move down";
-          "Mod1+Shift+K" = "move up";
-          "Mod1+Shift+L" = "move right";
+          "Mod1+v" = "floating toggle";
+          "Mod1+Shift+r" = "reload";
+          "Mod1+h" = "focus left";
+          "Mod1+j" = "focus down";
+          "Mod1+k" = "focus up";
+          "Mod1+l" = "focus right";
+          "Mod1+Shift+h" = "move left";
+          "Mod1+Shift+j" = "move down";
+          "Mod1+Shift+k" = "move up";
+          "Mod1+Shift+l" = "move right";
         };
       };
     };
